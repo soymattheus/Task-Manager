c0f980b6494362d343e8026ce971936f6f2515d4
Matheus Tavares <matheustavares1a@gmail.com>
2026-01-26 17:59:01 -0300

    Project init + feat/login and register

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,64 +1,109 @@
-import Image from "next/image";
+"use client";
+
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Input } from "@/components/ui/input";
+import { Field, FieldError, FieldLabel } from "@/components/ui/field";
+import { Button } from "@/components/ui/button";
+import { ButtonGroup } from "@/components/ui/button-group";
+import { Eye, EyeOff, ListCheck } from "lucide-react";
+import React from "react";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+
+const loginSchema = z.object({
+  email: z.email("Enter a valid email address.."),
+  password: z.string("Enter your password.").min(6, {
+    message: "Enter your password.",
+  }),
+});
+
+type LoginSchema = z.infer<typeof loginSchema>;
 
 export default function Home() {
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginSchema>({
+    resolver: zodResolver(loginSchema),
+  });
+  const [showPwd, setShowPwd] = React.useState<boolean>(false);
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+
+  async function onLogin(payload: LoginSchema) {
+    setIsLoading(true);
+    console.log(payload);
+    setIsLoading(false);
+  }
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="flex flex-col w-full min-h-screen items-center justify-between bg-white font-sans">
+      <header className="flex flex-row w-full py-4 px-6 border-b border-gray-300 justify-between items-center">
+        <div className="flex flex-row gap-2 p-1.5 rounded-md border border-gray-300">
+          <ListCheck />
+          Task Manager
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+        <Button onClick={() => router.push("/register")}>Register</Button>
+      </header>
+
+      <main className="flex flex-col w-full md:w-2/6 gap-4 justify-center items-center my-auto">
+        <p className="text-3xl font-bold">Login to your account</p>
+        <form
+          className="flex flex-col w-4/5 md:w-full border border-gray-300 gap-3 p-7 rounded-md"
+          onSubmit={handleSubmit(onLogin)}
+        >
+          {/* E-mail */}
+          <Field data-invalid={errors.email}>
+            <FieldLabel htmlFor="fieldgroup-email">E-mail</FieldLabel>
+            <Input
+              id="fieldgroup-email"
+              type="text"
+              placeholder="name@example.com"
+              {...register("email")}
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+            <FieldError>{errors?.email?.message}</FieldError>
+          </Field>
+
+          {/* Password */}
+          <Field data-invalid={errors.password}>
+            <FieldLabel htmlFor="input-button-group">Password</FieldLabel>
+            <ButtonGroup className="flex flex-1 w-full h-full outline-0 data-[disabled=true]:bg-[#F0F0F0]">
+              <Input
+                type={showPwd ? "password" : "text"}
+                id="input-button-group"
+                placeholder="Password"
+                autoComplete="off"
+                {...register("password")}
+              />
+              <Button
+                type="button"
+                variant="outline"
+                onClick={() => setShowPwd(!showPwd)}
+              >
+                {showPwd ? <EyeOff /> : <Eye />}
+              </Button>
+            </ButtonGroup>
+            <FieldError>{errors?.password?.message}</FieldError>
+          </Field>
+
+          <Button disabled={isLoading} type="submit">
+            {isLoading ? "Loading" : "Login"}
+          </Button>
+
+          <div className="flex flex-col md:flex-row w-full justify-between items-center">
+            <p className="font-normal text-[14px]">
+              Don&apos;t have an account yet?
+            </p>
+            <Link
+              href="/register"
+              className="font-bold text-[14px] underline cursor-pointer"
+            >
+              Register
+            </Link>
+          </div>
+        </form>
       </main>
     </div>
   );
---
+++ src/app/register/page.tsx
@@ -0,0 +1,168 @@
+"use client";
+
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Input } from "@/components/ui/input";
+import { Field, FieldError, FieldLabel } from "@/components/ui/field";
+import { Button } from "@/components/ui/button";
+import { ButtonGroup } from "@/components/ui/button-group";
+import { Eye, EyeOff, ListCheck } from "lucide-react";
+import React from "react";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+
+const registerSchema = z
+  .object({
+    email: z.email("Enter a valid email address.."),
+    password: z.string("Enter your password.").min(6, {
+      message: "Enter your password.",
+    }),
+    confirmPassword: z.string("Confirm your password.").min(6, {
+      message: "Confirm your password.",
+    }),
+    fullName: z
+      .string("Enter a valid name.")
+      .min(1, "Name is required.")
+      .refine((value) => value.trim().split(/\s+/).length >= 2, {
+        message: "Please, enter your name and lastname.",
+      })
+      .regex(/^[a-z A-Z]+$/, {
+        message: "The field should contain only letters.",
+      })
+      .trim(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match.",
+    path: ["confirmPassword"],
+  });
+
+type RegisterSchema = z.infer<typeof registerSchema>;
+
+export default function Register() {
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterSchema>({
+    resolver: zodResolver(registerSchema),
+  });
+  const [showPwd, setShowPwd] = React.useState<boolean>(false);
+  const [showConfirmPwd, setShowConfirmPwd] = React.useState<boolean>(false);
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+
+  async function onRegister(payload: RegisterSchema) {
+    setIsLoading(true);
+    console.log(payload);
+    setIsLoading(false);
+  }
+  return (
+    <div className="flex flex-col w-full min-h-screen items-center justify-between bg-white font-sans">
+      <header className="flex flex-row w-full py-4 px-6 border-b border-gray-300 justify-between items-center">
+        <div className="flex flex-row gap-2 p-1.5 rounded-md border border-gray-300">
+          <ListCheck />
+          Task Manager
+        </div>
+        <Button onClick={() => router.push("/")}>Login</Button>
+      </header>
+
+      <main className="flex flex-col w-full md:w-2/6 gap-4 justify-center items-center my-auto">
+        <p className="text-3xl font-bold">Register your account</p>
+        <form
+          className="flex flex-col w-4/5 md:w-full border border-gray-300 gap-3 p-7 rounded-md"
+          onSubmit={handleSubmit(onRegister)}
+        >
+          {/* E-mail */}
+          <Field data-invalid={errors.email}>
+            <FieldLabel htmlFor="fieldgroup-email">E-mail</FieldLabel>
+            <Input
+              id="fieldgroup-email"
+              type="text"
+              placeholder="name@example.com"
+              {...register("email")}
+            />
+            <FieldError>{errors?.email?.message}</FieldError>
+          </Field>
+
+          {/* Password */}
+          <Field data-invalid={errors.password}>
+            <FieldLabel htmlFor="input-button-password">Password</FieldLabel>
+            <ButtonGroup className="flex flex-1 w-full h-full outline-0 data-[disabled=true]:bg-[#F0F0F0]">
+              <Input
+                type={showPwd ? "text" : "password"}
+                id="input-button-password"
+                placeholder="Password"
+                autoComplete="off"
+                {...register("password")}
+              />
+              <Button
+                type="button"
+                variant="outline"
+                onClick={() => setShowPwd(!showPwd)}
+              >
+                {showPwd ? <Eye /> : <EyeOff />}
+              </Button>
+            </ButtonGroup>
+            <FieldError>{errors?.password?.message}</FieldError>
+          </Field>
+
+          {/* Confirm Password */}
+          <Field data-invalid={errors.password}>
+            <FieldLabel htmlFor="input-button-confirm-password">
+              Confirm password
+            </FieldLabel>
+            <ButtonGroup className="flex flex-1 w-full h-full outline-0 data-[disabled=true]:bg-[#F0F0F0]">
+              <Input
+                type={showConfirmPwd ? "text" : "password"}
+                id="input-button-confirm-password"
+                placeholder="Confirm Password"
+                autoComplete="off"
+                {...register("confirmPassword")}
+              />
+              <Button
+                type="button"
+                variant="outline"
+                onClick={() => setShowConfirmPwd(!showConfirmPwd)}
+              >
+                {showConfirmPwd ? <Eye /> : <EyeOff />}
+              </Button>
+            </ButtonGroup>
+            <FieldError>{errors?.confirmPassword?.message}</FieldError>
+          </Field>
+
+          <hr className="border-gray-300" />
+
+          {/* Name */}
+          <Field data-invalid={errors.fullName}>
+            <FieldLabel htmlFor="fieldgroup-fullName">Full name</FieldLabel>
+            <Input
+              id="fieldgroup-fullName"
+              type="text"
+              placeholder="Type your full name"
+              autoComplete="off"
+              {...register("fullName")}
+            />
+            <FieldError>{errors?.fullName?.message}</FieldError>
+          </Field>
+
+          <Button disabled={isLoading} type="submit">
+            {isLoading ? "Loading" : "Register"}
+          </Button>
+
+          <div className="flex flex-col md:flex-row w-full justify-between items-center">
+            <p className="font-normal text-[14px]">
+              Do you already have an account?
+            </p>
+            <Link
+              href="/"
+              className="font-bold text-[14px] underline cursor-pointer"
+            >
+              Login
+            </Link>
+          </div>
+        </form>
+      </main>
+    </div>
+  );
+}
